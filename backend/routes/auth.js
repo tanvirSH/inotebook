fdcce38787df0bd25af7348e5c854541b06677d8
tanvirSH <tanvit.iu14@gmail.com>
2022-04-21 03:58:42 +0530

    Create user using: POST 'api/auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const User = require('../models/User');
 
 const router = express.Router();
 
+//Create user using: POST 'api/auth'
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.json(req.body);
+});
 router.get('/', (req, res) => {
-    obj = {
-        a: 'hello A',
-        number: 786
-    }
-    res.json(obj);
+    console.log(req.body);
+    res.json(req.body);
 });
 
 module.exports = router;
