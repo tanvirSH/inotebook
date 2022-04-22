350bd9d89a7323629bc20263a61a488758652111
tanvirSH <tanvit.iu14@gmail.com>
2022-04-23 01:39:38 +0530

    Creating-ThunderClient-Collections express validation

--- backend/models/User.js
+++ backend/models/User.js
@@ -20,4 +20,5 @@ const UserSchema = new Schema({
     }
 });
 
-module.exports = mongoose.model('user', UserSchema);
+const User = mongoose.model('user', UserSchema);
+module.exports = User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,19 +1,52 @@
-const express = require('express');
-const User = require('../models/User');
+const express = require("express");
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
 const router = express.Router();
 
-//Create user using: POST 'api/auth'
+//Create user using: POST 'api/auth/createUser' no login required
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.json(req.body);
-});
-router.get('/', (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
+router.post(
+  "/createUser",
+  [
+    body("name", "Enter a valid name").isLength({ min: "3" }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be at least 5 characters").isLength({
+      min: "5",
+    }),
+  ],
+  async (req, res) => {
+    // If error return bad request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      // Check if user exist with same email
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry this email already exist!" });
+      }
+
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      res.json({ success: user });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error");
+    }
+  }
+);
+
+router.get("/", (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
 });
 
 module.exports = router;
