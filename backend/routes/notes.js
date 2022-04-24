3b373b421e18c01ce0774157a97196e8b5f6cb2b
tanvirSH <tanvit.iu14@gmail.com>
2022-04-24 17:08:09 +0530

    Endpoint-Deleting-Note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -58,15 +58,45 @@ router.put(
       if(description){newNote.description = description;}
       if(tag){newNote.tag = tag;}
 
+      //Find the note to be update
       let note = await Notes.findById(req.params.id);
+    
       if(!note){return res.status(400).send("Not found")}
 
+      //Do not allow user to update if user is different
       if(note.user.toString() !== req.user.id){
         return res.status(401).send("Not Allowed");
       }
       
+      // Update note
       note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true});
       res.json(note);
+    } catch (error) {
+      console.error(error.message);
+      //Respond if something happen to server
+      res.status(500).send("Internal server error");
+    }
+  }
+);
+
+//Route 4: Delete note: DELETE 'api/notes/deletenote' - login required
+router.delete(
+  "/deletenote/:id", fetchuser,
+  async (req, res) => {
+    try {
+      //Find the note to be delete 
+      let note = await Notes.findById(req.params.id);
+    
+      if(!note){return res.status(400).send("Not found")}
+
+      //Do not allow user to delete note if user is different
+      if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not Allowed");
+      }
+      
+      // Delete note
+      note = await Notes.findByIdAndDelete(req.params.id);
+      res.json({"Succes!": "Note has been deleted",  note:note});
     } catch (error) {
       console.error(error.message);
       //Respond if something happen to server
