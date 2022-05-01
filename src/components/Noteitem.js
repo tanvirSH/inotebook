c26ced361d036ff7ebd88c71e7f9527608b7e74b
tanvirSH <tanvit.iu14@gmail.com>
2022-05-02 03:29:07 +0530

    editNote-serviceIntegrataion

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -1,24 +1,34 @@
 import React, { useContext } from "react";
 import noteContext from "../context/notes/noteContext";
 
-
 const Noteitem = (props) => {
-  const { note } = props;
+  const { note, updatenote } = props;
   const context = useContext(noteContext);
-  const {deleteNote} = context;
+  const { deleteNote } = context;
   const deleteItem = (id) => {
     deleteNote(id);
-  }
+  };
   return (
     <div className="col-md-3">
       <div className="card my-3" style={{ width: "17rem" }}>
         <div className="card-body">
           <div className="d-flex align-items-center justify-content-between">
             <h5 className="card-title">{note.title}</h5>
-            <i className="fa-solid fa-pen-to-square"></i>
-            <i className="fa-solid fa-trash-can" onClick={() => {deleteItem(note._id)}}></i>
+            <i
+              className="fa-solid fa-pen-to-square"
+              onClick={() => {
+                updatenote(note);
+              }}
+            ></i>
+            <i
+              className="fa-solid fa-trash-can"
+              onClick={() => {
+                deleteItem(note._id);
+              }}
+            ></i>
           </div>
           <p className="card-text">{note.description}</p>
+          <p className="card-text">{note.tag}</p>
         </div>
       </div>
     </div>
